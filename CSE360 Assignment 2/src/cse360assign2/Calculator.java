614c5503777e9f5503089761b8589f898f0d9b1c
Ettien <Ettien@The-Red-King>
2019-02-23 21:58:56 -0700

    Initial Version
    First Commit

---
+++ CSE360 Assignment 2/src/cse360assign2/Calculator.java
@@ -0,0 +1,79 @@
+/**
+* Author: Ettien Martinez
+* Class ID: 476 
+* Assignment: Assignment 2
+* Description: This Calculator will conduct operations with the 
+* inputted variables to generate a total. The total will be added,
+* subtracted, multiplied, and divided. In addition the program will 
+* print the history of the operations conducted.
+*/
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**  
+	* 
+	*   
+	*/  
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**  
+	* Calculate total of operations conducted
+	* 
+	*/  
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**  
+	* add the parameter to the total variable
+	* 
+	* @param value Int variable which will be added
+	* to the total variable. 
+	*/  
+	public void add (int value) {
+		
+	}
+	
+	/**  
+	* subtract the parameter to the total variable
+	* 
+	* @param value Int variable which will be subtracted
+	* from the total variable. 
+	*/ 
+	public void subtract (int value) {
+		
+	}
+	
+	/**  
+	* multiply the parameter to the total variable
+	* 
+	* @param value Int variable which will be multiplied
+	* to the total variable. 
+	*/ 
+	public void multiply (int value) {
+		
+	}
+	
+	/**  
+	* divide the parameter to the total variable
+	* 
+	* @param value Int variable which the total variable
+	* will be divided by 
+	*/ 
+	public void divide (int value) {
+		
+	}
+	
+	/**  
+	* a history report of all the operations will be generated
+	*/ 
+	public String getHistory () {
+		return "";
+	}
+}
